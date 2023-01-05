0a1c10aa0820c7e7040f785b1b9525404178354a
sinhyeonho <gusgh4922@gmail.com>
2023-01-05 17:59:12 +0900

    init 활용 randomElement() 는 return값이 옵셔널이라 언래핑작업후 값추가

--- RandomRotto/MainView.swift
+++ RandomRotto/MainView.swift
@@ -8,83 +8,65 @@
 import SwiftUI
 
 struct MainView: View {
+    @State var number = [Int](1...45)
+    @State var randomNumber : [Int] = []
+    
+    init() {
+        CreateRandomNumber()
+    }
     var body: some View {
-        HStack{
+        VStack{
             HStack{
-                YellowCircle()
-                BlueCircle()
-                BlueCircle()
-                RedCircle()
-                GrayCircle()
-                GrayCircle()
+                HStack{
+                    CircleView(randomNumber[0], Color.yellow)
+                    CircleView(randomNumber[1], Color.yellow)
+                    CircleView(randomNumber[2], Color.yellow)
+                    CircleView(randomNumber[3], Color.yellow)
+                    CircleView(randomNumber[4], Color.yellow)
+                    CircleView(randomNumber[5], Color.yellow)
+                }
+                
             }
-            .background(Color.brown)
-            .cornerRadius(10)
-            HStack{
-                RedCircle()
+            .frame(width: 300,height: 60)
+            .background(Color.mint)
+            .cornerRadius(12)
+            .fixedSize()
+            Image(systemName: "plus")
+                .fontWeight(.bold)
+                .font(.system(size: 20))
+                .frame(width: 10,height: 10)
+                .foregroundColor(.gray)
+        }
+    }
+    
+    //랜덤번호 추출
+    func CreateRandomNumber() {
+        for _ in 0..<6{
+            if let n = number.randomElement(){
+                randomNumber.append(n)
             }
         }
-        
-        
     }
+    
 }
 
-struct YellowCircle: View{
-    var randomNumber = Int.random(in: 1...45)
-    var body: some View {
-        ZStack{
-            Circle()
-                .fill(Color.yellow)
-                .frame(width: 40,height: 40)
-            Text("\(randomNumber)")
-            
-                .foregroundColor(Color.white)
-                .font(.system(size: 21))
-        }
+struct CircleView : View{
+    @State var randomNumber : Int
+    @State var color : Color
+    init(_ randomNumber: Int, _ color: Color) {
+        self.randomNumber = randomNumber
+        self.color = color
     }
-}
-struct GrayCircle: View{
-    var randomNumber = Int.random(in: 1...45)
-    var body: some View {
+    var body: some View{
         ZStack{
             Circle()
-                .fill(Color.gray)
+                .fill(color)
                 .frame(width: 40,height: 40)
             Text("\(randomNumber)")
                 .foregroundColor(Color.white)
-                .font(.system(size: 21))
         }
     }
 }
-struct BlueCircle: View{
-    var randomNumber = Int.random(in: 1...45)
-    var body: some View {
-        ZStack{
-            Circle()
-                .fill(Color.blue)
-                .frame(width: 40,height: 40)
-                .shadow(radius: 2,x: 2,y:2)
-            Text("\(randomNumber)")
-                .foregroundColor(Color.white)
-                .font(.system(size: 21))
-        }
-    }
-}
-struct RedCircle: View{
-    var randomNumber = Int.random(in: 1...45)
-    var body: some View {
-        ZStack{
-            Circle()
-                .fill(Color.red)
-                .frame(width: 40,height: 40)
-                .shadow(radius: 2,x: 2,y:2)
-            Text("\(randomNumber)")
-                .foregroundColor(Color.white)
-                .font(.system(size: 21))
-        }
-    }
-}
-    
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
