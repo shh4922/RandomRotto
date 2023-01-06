95590b0e553e9dfb0b35d0cbe8fe338240b85b26
sinhyeonho <gusgh4922@gmail.com>
2023-01-07 04:34:29 +0900

    로또번호는 생성 완료

--- RandomRotto/MainView.swift
+++ RandomRotto/MainView.swift
@@ -1,53 +1,54 @@
-//
-//  MainView.swift
-//  RandomRotto
-//
-//  Created by HyeonHo on 2023/01/04.
-//
 
 import SwiftUI
 
 struct MainView: View {
-    @State var number = [Int](1...45)
-    @State var randomNumber : [Int] = []
-    
+    var randomNumberSorted : [Int] = []
+    var number = [Int](1...45)
+    var randomNumber : Set<Int> = []
     init() {
-        CreateRandomNumber()
+        randomNumberSorted = CreateRandomNumber(randomNumber)
     }
+    
     var body: some View {
         VStack{
             HStack{
                 HStack{
-                    CircleView(randomNumber[0], Color.yellow)
-                    CircleView(randomNumber[1], Color.yellow)
-                    CircleView(randomNumber[2], Color.yellow)
-                    CircleView(randomNumber[3], Color.yellow)
-                    CircleView(randomNumber[4], Color.yellow)
-                    CircleView(randomNumber[5], Color.yellow)
-                }
+                    CircleView(randomNumberSorted[0], Color.yellow)
+                    CircleView(randomNumberSorted[1], Color.blue)
+                    CircleView(randomNumberSorted[2], Color.blue)
+                    CircleView(randomNumberSorted[3], Color.red)
+                    CircleView(randomNumberSorted[4], Color.gray)
+                    CircleView(randomNumberSorted[5], Color.gray)
+                }.frame(width: 300,height: 55)
+                    .background(Color.mint)
+                    .cornerRadius(12)
+                    .fixedSize()
                 
+                Image(systemName: "plus")
+                    .fontWeight(.bold)
+                    .font(.system(size: 20))
+                    .frame(width: 10,height: 10)
+                    .foregroundColor(.gray)
+                HStack{
+                    BonusCircle(number.randomElement(), Color.red)
+                }
+                .frame(width: 50,height: 55)
+                .background(Color.mint)
+                .cornerRadius(10)
             }
-            .frame(width: 300,height: 60)
-            .background(Color.mint)
-            .cornerRadius(12)
-            .fixedSize()
-            Image(systemName: "plus")
-                .fontWeight(.bold)
-                .font(.system(size: 20))
-                .frame(width: 10,height: 10)
-                .foregroundColor(.gray)
         }
     }
     
     //랜덤번호 추출
-    func CreateRandomNumber() {
-        for _ in 0..<6{
-            if let n = number.randomElement(){
-                randomNumber.append(n)
+    func CreateRandomNumber(_ randomNumber : Set<Int>) -> [Int]{
+        var randomNumber : Set<Int> = []
+        while randomNumber.count < 6{
+            if let nn = number.randomElement(){
+                randomNumber.insert(nn)
             }
         }
+        return randomNumber.sorted()
     }
-    
 }
 
 struct CircleView : View{
@@ -55,6 +56,26 @@ struct CircleView : View{
     @State var color : Color
     init(_ randomNumber: Int, _ color: Color) {
         self.randomNumber = randomNumber
+        self.color = color
+
+    }
+    var body: some View{
+        ZStack{
+            Circle()
+                .fill(color)
+                .frame(width: 40,height: 40)
+            Text("\(randomNumber)")
+                .foregroundColor(Color.white)
+        }
+    }
+}
+struct BonusCircle : View{
+    @State var randomNumber : Int = 1
+    @State var color : Color
+    init(_ randomNumber: Int?, _ color: Color) {
+        if let n = randomNumber{
+            self.randomNumber = n
+        }
         self.color = color
     }
     var body: some View{
@@ -67,6 +88,9 @@ struct CircleView : View{
         }
     }
 }
+
+
+
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
