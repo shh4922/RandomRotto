2052d4ed02bac68260be835cfc05adddc49366bf
sinhyeonho <gusgh4922@gmail.com>
2023-01-08 23:50:32 +0900

    buttonChange

--- RandomRotto/MainView.swift
+++ RandomRotto/MainView.swift
@@ -43,6 +43,10 @@ struct MainView: View {
             }){
                 Text("reset")
             }
+            .frame(width: 100,height: 40)
+            .background(Color.green)
+            .cornerRadius(12)
+            .foregroundColor(Color.white)
         }
     }
     
